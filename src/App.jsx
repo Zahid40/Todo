a7127c3e6c8ef943716030f0ce9a7a34368b8e68
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-03-13 00:36:56 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center">
+        <div className="max-w-[600px] min-w-96 w-full">
+          <Navbar />
+          <div className="w-full aspect-video  p-24">
+            <div className="aspect-video w-full p-8 rounded-2xl border border-[var(--secondary)] "></div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+     <div className="w-full bg-inherit flex justify-between items-center p-4 shadow-sm text-white">
+        <h3 className='text-white font-medium'>Todoz</h3>
+        <div>
+            gd
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Navbar
