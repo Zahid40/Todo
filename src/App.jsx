0d68f630034ea9d0159caf0d71e3a2b7e4a36b82
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-03-15 00:52:33 +0530

    150324 - Design Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,62 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Todo from "./components/Todo";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const data = [
+    {
+      title: "jsx shikh lunga",
+      description: "Decs 1",
+    },
+    {
+      title: "Hi 2 jdfhdj fdgh",
+      description: "Desc 2",
+    },
+  ];
+
   return (
     <>
       <div className="flex justify-center">
-        <div className="max-w-[600px] min-w-96 w-full">
+        <div className="max-w-[600px] min-w-96 w-full flex flex-col gap-8">
           <Navbar />
-          <div className="w-full aspect-video  p-24">
-            <div className="aspect-video w-full p-8 rounded-2xl border border-[var(--secondary)] "></div>
+          <div className="px-24 flex flex-col gap-8">
+            <div className="w-full aspect-video  ">
+              <div className="aspect-video w-full p-8 rounded-[3rem] border border-[var(--secondary)] flex justify-evenly">
+                <div className="h-full aspect-square text-[var(--secondary)] flex flex-col justify-center ">
+                  <h1 className="text-2xl font-bold">Todo Done</h1>
+                  <p>Keep it up</p>
+                </div>
+                <div className="bg-[var(--primary)] h-full aspect-square rounded-full flex justify-center items-center">
+                  <h1 className="font-bold text-3xl">1/3</h1>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-stretch items-center gap-4">
+              <input
+              placeholder="write your next task"
+                type="text"
+                name="addNote"
+                id="addNote"
+                className="rounded-xl py-2 px-4 bg-gray-800 w-full placeholder:text-[var(--secondary)] placeholder:opacity-25 text-[var(--secondary)] border border-transparent focus:border focus:border-red-500"
+              />
+              <button className="flex items-center justify-center bg-[var(--primary)] aspect-square h-full rounded-full font-bold">
+                <span className="material-symbols-outlined " >add</span>
+              </button>
+            </div>
+            <div className="flex flex-col gap-4">
+              {data.map((todo) => {
+                return (
+                  <Todo
+                    key={todo.title}
+                    title={todo.title}
+                    description={todo.description}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,11 @@ function Navbar() {
   return (
     <>
      <div className="w-full bg-inherit flex justify-between items-center p-4 shadow-sm text-white">
-        <h3 className='text-white font-medium'>Todoz</h3>
-        <div>
-            gd
+        <h3 className='text-white font-bold text-xl'>Todo<span className='text-[var(--primary)]'>Z</span></h3>
+        <div className='flex gap-8'>
+           <a href="/"><img src="https://cdn-icons-png.flaticon.com/128/2111/2111432.png" className='w-6 invert' alt="" /></a>
+           <a href="/"><img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" className='w-6' alt="" /></a>
+           <a href="/"><img src="https://lh3.googleusercontent.com/a/ACg8ocJxADMIkIBvfexyntm-vXTsASjSikBYNQoM5X-Lta8KIB4=s96-c" className='rounded-full w-6' alt="" /></a>
         </div>
      </div>
     </>
