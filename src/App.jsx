05ccad9117fdf4d09f39fe45f3eba110abae608d
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-03-16 23:54:26 +0530

    160324

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Todo from "./components/Todo";
 import Footer from "./components/Footer";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todo, setTodo] = useState("");
@@ -19,15 +19,33 @@ function App() {
     },
   ];
 
-  const handleAdd = () => {
-    setTdata([...tdata , { todo , iscompleted : false}])
-    setTodo("")
+  const handleAdd = async () => {
+    setTdata([...tdata, { id: uuidv4(), todo, iscompleted: false }])
+    setTodo("");
     console.log(tdata);
   };
 
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setTodo(e.target.value);
-  }
+  };
+
+  const handledelete = (e , id) => {
+    let newTdata = tdata.filter(item=>{
+      return item.id!=id;
+    });
+    setTdata(newTdata);
+  };
+
+
+  const hangeCheckbox = (e) => {
+    let id = e.target.name;
+    let index = tdata.findIndex(item=>{
+      return item.id === id;
+    });
+    let newTdata = [...tdata];
+    newTdata[index].iscompleted = !newTdata[index].iscompleted;
+    setTdata(newTdata);
+  };
 
   return (
     <>
@@ -42,7 +60,7 @@ function App() {
                   <p>Keep it up</p>
                 </div>
                 <div className="bg-[var(--primary)] h-full aspect-square rounded-full flex justify-center items-center">
-                  <h1 className="font-bold text-3xl">2/3</h1>
+                  <h1 className="font-bold text-3xl"></h1>
                 </div>
               </div>
             </div>
@@ -66,18 +84,69 @@ function App() {
             <div className="flex flex-col gap-4">
               {tdata.map((val) => {
                 return (
-                  <Todo
-                    key={val.todo}
-                    title={val.todo}
-                    iscompleted={val.iscompleted}
-                  />
+                  <div key={val.id} className="flex justify-between items-center border border-[var(--secondary)] py-2 px-4 rounded-xl">
+                    <div className="flex justify-center items-center gap-4">
+                      <div className="inline-flex items-center">
+                        <label
+                          className="relative flex items-center p-3 rounded-full cursor-pointer"
+                          htmlFor="green"
+                        >
+                          <input
+                            onChange={hangeCheckbox}
+                            name={val.id}
+                            type="checkbox"
+                            className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-green-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-green-500 checked:before:bg-green-500 hover:before:opacity-10"
+                            id="green"
+                          />
+                          <span className="absolute text-green-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-3.5 w-3.5"
+                              viewBox="0 0 16 16"
+                              fill="currentColor"
+                            >
+                              <circle
+                                data-name="ellipse"
+                                cx="8"
+                                cy="8"
+                                r="8"
+                              ></circle>
+                            </svg>
+                          </span>
+                        </label>
+                      </div>
+
+                      <h2
+                        className={`text-[var(--secondary)] font-semibold text-lg ${
+                          val.iscompleted ? "line-through" : ""
+                        }`}
+                      >
+                        {val.todo}
+                      </h2>
+                    </div>
+                    <div className="flex justify-center items-center gap-4">
+                      <button className="flex justify-center items-center">
+                        <span className="material-symbols-outlined text-[var(--secondary)]">
+                          edit_square
+                        </span>
+                      </button>
+                      <button
+                        className="flex justify-center items-center"
+                        onClick={(e)=>{handledelete(e , val.id)}}
+                      >
+                        <span className="material-symbols-outlined text-[var(--secondary)]">
+                          delete
+                        </span>
+                      </button>
+                    </div>
+                  </div>
                 );
               })}
             </div>
           </div>
         </div>
       </div>
-      <Footer />
+      
     </>
   );
 }
