ad4f7f7092f3020184d233199e1237c3e71e4239
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-03-20 10:08:31 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <>
      <div className="w-full bg-inherit flex justify-between items-center p-4 shadow-sm text-white">
-        <h3 className='text-white font-bold text-xl'>Todo<span className='text-[var(--primary)]'>Z</span></h3>
+        <h3 className='text-white font-bold text-xl'>Doto<span className='text-[var(--primary)]'>Z</span></h3>
         <div className='flex gap-8'>
            <a href="https://github.com/Zahid40"><img src="https://cdn-icons-png.flaticon.com/128/2111/2111432.png" className='w-6 invert' alt="" /></a>
            <a href="https://www.instagram.com/its_zahid83/"><img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" className='w-6' alt="" /></a>
