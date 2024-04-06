3775c1a428e0913be0a03add5b780b89c3bc3fca
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-04-06 10:41:07 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className="flex flex-col gap-1 justify-center items-center relative left-0 bottom-0 h-16 w-full p-4 bg-gray-800 text-[var(--secondary)] text-xs">
+      <div className="flex flex-col gap-1 justify-center items-center fixed left-0 bottom-0 h-16 w-full p-4 bg-gray-800 text-[var(--secondary)] text-xs">
         <span>
           Created by <a href="https://zahid40.github.io/" className="text-[var(--primary)] underline ">Zahid</a>
         </span>
-        <span>Designed by <a href="https://dribbble.com/shots/22604632-Todo-Website-Todo-App?utm_source=Clipboard_Shot&utm_campaign=mbiburhan6&utm_content=Todo%20Website%20%2C%20Todo%20App&utm_medium=Social_Share" className="text-[var(--primary)] underline ">Burhanul Islam</a></span>
+        <span>A React App</span>
       </div>
     </>
   );
